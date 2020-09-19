70e1dba3b1e766aaf586659cb8a32673254100fe
nxtlo <35225215+nxtlo@users.noreply.github.com>
2020-09-19 22:02:33 +0300

    members join and leaving
    simple message when members join and leave your server 
    * Note: Change the channel id to a channel in your server

---
+++ cogs/join_leave.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+class Join_Leave(commands.Cog):
+    """Welcomes members on join, and leaving"""
+
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+
+#joining
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.bot.get_channel(411804307302776835)
+        await channel.send(f'Welcome to the server {member.mention}')
+# leaving
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = self.bot.get_channel(411804307302776835)
+        await channel.send(f'Member has just left {member.mention}')
+
+def setup(bot):
+    bot.add_cog(Join_Leave(bot))
