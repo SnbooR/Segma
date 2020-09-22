ccd1d6f0d9e126b086a162e5193e5286d527fc1b
nxtlo <35225215+nxtlo@users.noreply.github.com>
2020-09-22 07:34:26 +0300

    chunky update
    1: using [pretty_help](https://github.com/stroupbslayen/discord-pretty-help) to change the look of the built in help command and make it look better
    2: allows the bot to send messages when he go online!
    3: print the bot name and id and the servers he's in when launching bot.py
    4: bot activities 
    5: allows the owner of the bot only load cogs and unload them

--- bot.py
+++ bot.py
@@ -1,43 +1,54 @@
 import discord
 import os
 from discord.ext import commands
+import psutil
+from pretty_help import Navigation, PrettyHelp, __version__
+                   # you can change the prefix from here
+bot = commands.Bot(command_prefix="YOUR_PREFIX", help_command=PrettyHelp())
 
-bot = commands.Bot(command_prefix='>')
-bot.remove_command('help')
 
-
+@bot.event
 async def on_ready():
-    print(f'\n\nBot is ready!\nName: {bot.user.name}\nID: {bot.user.id}\n      ---------\n')
-
-    return
+        
+    print('Logged in as:\n')
+    print('Bot name:\n', bot.user.name)
+    print('Bot id:\n',bot.user.id)
+    print('Discord Version:\n',discord.__version__) 
+    print('------')
+    print('Servers connected to:')
+    
+    # whats the bot listening to, change the 'YOUR_BOT_STATUS'
+    activity = discord.Game(name="YOUR_BOT_STATUS", type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    
+    # allow the bot to send a message when he go online.
+    channel = bot.get_channel(YOUR_CHANNEL_ID)
+    await channel.send("I'm online!")
+    
+    for guild in bot.guilds:
+        print(guild.name, "\n")
 
 # Load and unload cogs
 
 @bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}') #loads the extension in the "cogs" folder
-    await ctx.send(f'Loaded "{extension}"')
-    print(f'Loaded `"{extension}"`')
-
-    return
-
+@commands.is_owner()
+async def load(ctx, extension, hidden=True):
+        bot.load_extension(f'cogs.{extension}') #loads the extension in the "cogs" folder
+        await ctx.send(f'Loaded "{extension}"')
+        print(f'Loaded `"{extension}"`')
 
 @bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}') #unloads the extension in the "cogs" folder
-    await ctx.send(f'Unloaded "{extension}"')
-    print(f'Unoaded `"{extension}"`')
+@commands.is_owner()
+async def unload(ctx, extension, hidden=True):
+        bot.unload_extension(f'cogs.{extension}') #unloads the extension in the "cogs" folder
+        await ctx.send(f'Unloaded "{extension}"')
+        print(f'Unoaded `"{extension}"`')
 
-    return
-
+# only the owner of the bot can load and unload cogs, you can change it to a specific role or else
 print('\n')
-
-
-
 
 for filename in os.listdir('./cogs'): #loads all files (*.py)
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}') #loads the file without ".py" for example: cogs.ping
-        print(f'Loaded {filename[:-3]}')
 
-bot.run('YOUR_TOKEN_HERE')
+bot.run('YOUR_BOT_TOKEN')
